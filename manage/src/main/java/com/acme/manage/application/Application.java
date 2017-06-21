4df9fc20281cfd10e5e1de56ae6ea1e13f325ffe
lcs <lcs0816@163.com>
2017-06-21 21:43:56 +0800

    init

---
+++ manage/src/main/java/com/acme/manage/application/Application.java
@@ -0,0 +1,26 @@
+package com.acme.manage.application;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.acme.manage")
+@ImportResource({ "classpath:META-INF/spring/applicationContext.xml" })
+public class Application extends SpringBootServletInitializer{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
