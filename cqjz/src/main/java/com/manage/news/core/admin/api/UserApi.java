a02fac7d55ca56b6f57d5f5939df72cc25b988c8
lcs <lcs0816@163.com>
2017-07-09 23:21:52 +0800

    angularjs

--- cqjz/src/main/java/com/manage/news/core/admin/api/UserApi.java
+++ cqjz/src/main/java/com/manage/news/core/admin/api/UserApi.java
@@ -12,9 +12,15 @@ import com.manage.news.spring.annotation.TokenPermission;
 import com.manage.news.spring.annotation.TokenPermissionGroup;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Api(value = "User", tags = "User manage")
@@ -25,19 +31,19 @@ public class UserApi extends BaseServiceApi {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
-    public ResponseInfo login(String user, String password){
+    @PostMapping("login")
+    public ResponseInfo login(HttpServletRequest request, String account, String password) {
         TokenUser tokenUser = new TokenUser();
-        tokenUser.setAccount(user);
+        tokenUser.setAccount(account);
         ResponseInfo response = new ResponseInfo();
         response.setStatus(ResponseEnum.SUCCESS);
-        userService.login(user, password);
+        userService.login(account, password);
         return response;
     }
 
     @TokenCheck
     @RequestMapping("/register")
-    public ResponseInfo register(UserDto userDto){
+    public ResponseInfo register(UserDto userDto) {
 
         ResponseInfo response = new ResponseInfo();
         userService.add(userDto);
