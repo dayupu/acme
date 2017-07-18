4aaa9848bf45fd135d88bd9ae2e3df56191db0b0
bert luo <bert.luo@otms.cn>
2017-07-18 18:29:28 +0800

    add directive

--- cqjz/src/main/resources/static/js/app.js
+++ cqjz/src/main/resources/static/js/app.js
@@ -1,6 +1,23 @@
 var menuApp = angular.module("menuApp",[]);
-var mainApp = angular.module("mainApp",["menuApp"]);
+menuApp.directive("myDirective",function(){
+  return {
+    scope:{
+      title:"@"
+    },
+    template: function(){
+      return '{{title}}';
+    },
+    link: function(scope, element, attrs){
+          element.ready(function(){
+          alert(scope.title);
+        });
+    }
+   };
+});
 
+var mainApp = angular.module("mainApp",["menuApp"]);
+var defaultMenus = [{"name":"menu01","childrens":[{"name":"childmenu"}]},{"name":"menu02"},{"name":"menu03"},{"name":"menu04"}]
 menuApp.controller("menuCtrl",function($scope){
     $scope.name = "aa";
+    $scope.menus=defaultMenus;
 });
