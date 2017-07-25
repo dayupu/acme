1c3257857a027298efe731bdfddbdcf9a67a2d18
bert luo <bert.luo@otms.cn>
2017-07-25 13:36:01 +0800

    modify class name

--- cqjz/src/main/resources/static/js/app.js
+++ cqjz/src/main/resources/static/js/app.js
@@ -1,6 +1,7 @@
 var asideApp = angular.module("asideApp", []);
 var mainApp = angular.module("mainApp", ["asideApp"]);
 asideApp.controller("asideController", function ($http, $scope) {
+    $scope.isShow=false;
     $scope.loadMenu = function () {
         $http.get("/admin/index/menuList").then(function successCallback(response) {
             if (requestSuccess(response.data.status)) {
@@ -9,6 +10,15 @@ asideApp.controller("asideController", function ($http, $scope) {
                 $scope.message = response.data.message;
             }
         });
+    }
+    $scope.menuClick = function(event, menu){
+        var secondMenu = $(event.target).next("ul");
+        if($(secondMenu).length > 0){
+           menu.extend = !menu.extend;
+           $(secondMenu).slideToggle("fast");
+        }else{
+           //alert(menu.name);
+        }
     }
 });
 
