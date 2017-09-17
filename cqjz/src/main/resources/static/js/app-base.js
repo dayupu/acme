939f7591a5ddcff0febc10c326ad994fa27d34c6
shenjun <shenjun@168.com>
2017-09-17 23:48:45 +0800

    adjust style

---
+++ cqjz/src/main/resources/static/js/app-base.js
@@ -0,0 +1,131 @@
+var _appContextPath = null;
+
+// 拼接项目路径
+function fullPath(path) {
+    if (_appContextPath == null) {
+        _appContextPath = $("#appContextPath").val();
+    }
+    return _appContextPath + path;
+}
+
+// 验证响应信息
+function verifyData(data) {
+    if (data.status == "1000") {
+        return true;
+    }
+    return false;
+}
+
+// 创建Umeditor
+function createUeditor(elementId) {
+    umeditorInit($);
+    return UM.getEditor(elementId);
+}
+
+mainApp.controller("menuController", function ($http, $scope, $location, $sessionStorage, $state, mineMessage, mineHttp) {
+    $scope.loadMenu = function () {
+        mineHttp.send("GET", "admin/index/menuList", {}, function (data) {
+            if (verifyData(data)) {
+                $scope.buildMenu(data.content);
+                $scope.menuListener();
+            } else {
+                $scope.message = data.message;
+            }
+        });
+    };
+    $scope.buildMenu = function (menus) {
+        var pNode = $("ul[mark='menuRoot']");
+        var html = "";
+        for (var i in menus) {
+            var menu = menus[i];
+            if (menu.subMenus.length > 0) {
+                var sub = "<ul class='menu-nav menu-second-ul' style='display:block;'>";
+                for (var j in menu.subMenus) {
+                    var subMenu = menu.subMenus[j];
+                    sub += "<li mark='menuTwo'><a href='javascript:void(0);' class='second-menu-a' url='" + subMenu.url + "'>" + subMenu.name + "</a></li>";
+                }
+                sub += "</ul>";
+            }
+            html += "<li mark='menuOne'><a href='javascript:void(0);' class='first-menu-a'><span class='pull-right'><i class='fa fa-angle-right'></i></span>" +
+                "<i class='fa fa-bars' style='font-size: 20px;'></i>&nbsp;&nbsp;&nbsp;&nbsp;" + menu.name + "</span></a>"
+                + sub + "</li>";
+        }
+        $(pNode).append(html);
+    };
+
+    $scope.menuArrow = function (obj, scope) {
+        if (scope == "auto") {
+            if ($(obj).hasClass("fa-angle-right")) {
+                $(obj).removeClass("fa-angle-right");
+                $(obj).addClass("fa-angle-down");
+            } else {
+                $(obj).removeClass("fa-angle-down");
+                $(obj).addClass("fa-angle-right");
+            }
+        } else if (scope = "hide") {
+            $(obj).removeClass("fa-angle-down");
+            $(obj).addClass("fa-angle-right");
+        }
+
+    };
+    $scope.menuListener = function () {
+        var rootNode = $("ul[mark='menuRoot']");
+        $(rootNode).children("li[mark='menuOne']").each(function () {
+            var oneA = $(this).children("a");
+            var oneUL = $(this).children("ul");
+            $scope.hoverEvent(oneA);
+            $(oneA).click(function () {
+                $(rootNode).children("li[mark='menuOne']").each(function () {
+                    if (!$(this).is($(oneA).parent())) {
+                        $scope.menuArrow($(this).find("i[class^='fa']")[0], "hide");
+                        $(this).children("ul").slideUp("fast");
+                    }
+                });
+                $scope.menuArrow($(this).find("i[class^='fa']")[0], "auto");
+                $(this).next("ul").slideToggle("fast");
+            });
+            $(oneUL).children("li[mark='menuTwo']").each(function () {
+                var twoA = $(this).children("a");
+                $scope.hoverEvent(twoA);
+                $(twoA).click(function () {
+                    $scope.clickEvent(twoA);
+                    $state.go($(this).attr("url"));
+                });
+            });
+        });
+    };
+
+    $scope.hoverEvent = function (obj) {
+        $(obj).hover(function () {
+            if (!$(obj).hasClass("menu-hover")) {
+                $(obj).addClass("menu-hover");
+            }
+        }, function () {
+            if ($(obj).hasClass("menu-hover")) {
+                $(obj).removeClass("menu-hover");
+            }
+        });
+    };
+
+    $scope.clickEvent = function (obj) {
+        if (!$(obj).hasClass("menu-selected")) {
+            $(obj).addClass("menu-selected");
+        }
+        var curParent = $(obj).parent();
+        $("li[mark='menuTwo']").not(curParent).each(function () {
+            if ($(this).children("a").hasClass("menu-selected")) {
+                $(this).children("a").removeClass("menu-selected");
+            }
+        })
+    };
+});
+mainApp.controller("headerController", function ($http, $scope, $location, $sessionStorage, $state, mineMessage, mineHttp) {
+    $scope.loginOut = function () {
+        location.href = fullPath("admin/loginOut");
+    }
+});
+mainApp.controller("contentController", function ($scope, mineMessage, $sessionStorage, $location) {
+    mineMessage.subscribe("menuLocation", function (event, menu) {
+        $scope.navLocations = menu.locations;
+    });
+});
---
+++ cqjz/src/main/resources/static/js/app-directive.js
@@ -0,0 +1,99 @@
+mainApp.directive("mineAction", function () {
+    return {
+        restrict: 'E',
+        scope: {
+            name: "@",
+            action: "&",
+            icon: "@"
+        },
+        template: "<span class='mine-action'><a href='javascript:void(0);' ng-click='action()'><i ng-if='icon != null' ng-class='icon'></i>{{name}}</a></span>",
+        replace: true
+    }
+});
+mainApp.directive("mineValidator", function () {
+    return {
+        restrict: 'E',
+        scope: {
+            ok: "@",
+            error: "@",
+            valid: "=",
+            when: "=",
+            right: "="
+        },
+        template: "<span><span class='help-inline' style='color:#a94442;' ng-show='(when == null || when) && !valid'><i class='fa fa-exclamation-circle'></i><span class='message'>{{error}}</span></span>" +
+        "<span ng-if='right' class='help-inline' style='color:green;' ng-show='(when == null || when) && valid'><i class='fa fa-check-circle-o'></i><span class='message'>{{ok}}</span></span></span>",
+        replace: true
+    }
+});
+mainApp.directive("mineRequired", function () {
+    return {
+        restrict: 'EA',
+        template: "<span class='mine-required'>*</span>",
+        replace: true
+    }
+});
+mainApp.directive("mineUmeditor", function ($rootScope) {
+    return {
+        restrict: 'EA',
+        require: 'ngModel',
+        link: function (scope, ele, attrs, ngModel) {
+            var ctrl = {
+                initialized: false,
+                editorInstance: null,
+                placeholder: attrs['metaUmeditorPlaceholder'] || '',
+                focus: false
+            };
+
+            var height = attrs["height"];
+            if (typeof height == "undefined") {
+                height = 200;
+            }
+            var setting = {initialFrameHeight: height};
+            ctrl.init = function () {
+                ctrl.createEditor();
+                ngModel.$render = function () {
+                    if (ctrl.initialized) {
+                        ctrl.editorInstance.ready(function () {
+                            ctrl.editorInstance.setContent(ngModel.$viewValue || '');
+                            ctrl.checkPlaceholder();
+                        });
+                    }
+                };
+            };
+            ctrl.createEditor = function () {
+                if (!ctrl.initialized) {
+                    umeditorInit($);
+                    ctrl.editorInstance = UM.getEditor(attrs['id'], setting);
+                    ctrl.initialized = true;
+                    ctrl.initListener();
+                }
+            };
+            //修改ngModel Value
+            ctrl.updateModelView = function () {
+                var modelContent = ctrl.editorInstance.getContent();
+                ngModel.$setViewValue(modelContent);
+                if (!scope.$root.$$phase) {
+                    scope.$apply();
+                }
+            };
+            //监听多个事件
+            ctrl.initListener = function () {
+                ctrl.editorInstance.addListener('contentChange', function () {
+                    scope.$evalAsync(ctrl.updateModelView);
+                });
+                ctrl.editorInstance.addListener('fullscreenchanged', function (event, isFullScreen) {
+                    if (!isFullScreen) {
+                        ctrl.editorInstance.setHeight(height);
+                        $("body").css("overflow-y","auto");
+                    }
+                });
+            };
+            ctrl.checkPlaceholder = function () {
+            };
+            ctrl.init();
+            $rootScope.$on('$locationChangeStart', function (event) {
+                ctrl.editorInstance && ctrl.editorInstance.destroy();
+            });
+        }
+    }
+});
