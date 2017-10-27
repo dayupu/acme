d8a7e49803a57bb582fd4402f02db91b095bab95
shenjun <shenjun@168.com>
2017-10-27 19:55:22 +0800

    anyone

---
+++ cqjz/src/main/resources/static/anyone/app-home.js
@@ -0,0 +1,3 @@
+function goto(url) {
+    location.href = url;
+}
--- cqjz/src/main/resources/static/anyone/app.js
+++ cqjz/src/main/resources/static/anyone/app.js
@@ -1,6 +1,16 @@
-var anyoneApp = angular.module("anyoneApp",['ngRoute']);
-anyoneApp.config(['$routeProvider', function($routeProvider){
+var anyoneApp = angular.module("anyoneApp", ['ngRoute']);
+anyoneApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-        .when('/',{templateUrl:'./anyone/home.htm'})
-        .otherwise({redirectTo:'/'});
+        .when('/', {templateUrl: './anyone/home.htm'})
+        .when('/contacts', {templateUrl: './anyone/contacts.htm'})
+        .when('/jzfz', {templateUrl: './anyone/jzfz.htm'})
+        .when('/bmjy', {templateUrl: './anyone/bmjy.htm'})
+        .otherwise({redirectTo: '/'});
 }]);
+
+/*网页头部操作*/
+anyoneApp.controller("headerController", function ($scope, $location) {
+    $scope.gotToUrl = function (url) {
+        $location.path(url)
+    }
+});
